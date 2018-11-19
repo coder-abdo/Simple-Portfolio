67cb5ac27f896a64a4f178e6bba27786bcf0fb47
coder-abdo <a.h2020.2007@gmail.com>
2018-11-20 00:18:38 +0200

    Adding our-work & our-service and counting section

--- app/app.js
+++ app/app.js
@@ -1,5 +1,30 @@
 "use strict";
 (function (win, doc, $) {
+    class Counter {
+        constructor (start, end, elm, time) {
+          this.start = start;
+          this.end = end;
+          this.elm = document.querySelector(elm);
+          this.time = time;
+        }
+        run () {
+          let t0 = new Date(),
+              self = this;
+          this.update(0);
+          requestAnimationFrame(function update(){
+            let t = new Date(),
+                dt = t - t0;
+            self.update(Math.min(dt / self.time, 1));
+            if (dt < self.time) {
+              requestAnimationFrame(update);
+            }
+          });
+        }
+        update (t) {
+          t = Math.round(this.start + (this.end - this.start) * t);
+          this.elm.textContent = t;
+        }
+      }
     $('.nav__btn').on('click', function(e){
         $(this).toggleClass('show');
         $('.nav__list').toggleClass('hide');
@@ -12,9 +37,11 @@
         loop: true
       });
       var skillsTop = $('#about').offset().top;
+      var countingTop = $('.counting').offset().top;
       $(win).on('scroll', function () {
         makeNavFixed();
         makeSkillMove();
+        makeCounting();
       });
       function makeNavFixed(){
           if($(win).scrollTop() >= 100){
@@ -35,5 +62,26 @@
                 }, 300);
               });
           }
+      }
+      function makeCounting() {
+        var options = {
+            useEasing: true,
+            useGrouping: true,
+            separator: null,
+            decimal: null
+        };
+        
+        // Find all Statistics on page, put them inside a variable
+        var statistics = $(".num");
+        if($(win).scrollTop() >= countingTop){
+            var counter1 = new Counter(0, 895, '#count-1', 1500);
+            var counter2 = new Counter(0, 1200, '#count-2', 1500);
+            var counter3 = new Counter(0, 968, '#count-3', 1500);
+            var counter4 = new Counter(0, 1082, '#count-4', 1500);
+            counter1.run();
+            counter2.run();
+            counter3.run();
+            counter4.run();
+        }
       }
 })(window, document, jQuery);
